b4fae592d6a16b87c9763f82f79a1c75f3f2d29d
MuLuu09 <muluu09@gmail.com>
2016-11-01 03:45:35 +0800

    Create reset.sh

---
+++ reset.sh
@@ -0,0 +1,8 @@
+service ssh restart
+service openvpn restart
+service dropbear restart
+service nginx restart
+service php5-fpm restart
+service webmin restart
+service squid3 restart
+service fail2ban restart
